df3e322bd83b886e5fbd675bf7aae03b1b769f3f
PattimuraDev <dwisatriapatra2000@gmail.com>
2022-09-07 14:01:18 +0700

    Initial commit

---
+++ src/codewars/kyu_8/BeginnerReduceButGrow.java
@@ -0,0 +1,21 @@
+package codewars.kyu_8;
+
+public class BeginnerReduceButGrow {
+    /**
+     * Fungsi grow untuk menentukan hasil perkalian semua komponen array secara berurutan
+     * @param x parameter untuk sebuah array integer
+     * @return hasil perkalian semua komponen array
+     */
+    public static int grow(int[] x){
+        int result = 1;
+        for (int j : x) {
+            result *= j;
+        }
+        return result;
+    }
+
+//    public static void main(String[] args) {
+//        int [] a = {1, 3, 5, 2};
+//        System.out.println(grow(a));
+//    }
+}
---
+++ src/codewars/kyu_8/GrasshoperCheckForFactor.java
@@ -0,0 +1,17 @@
+package codewars.kyu_8;
+
+public class GrasshoperCheckForFactor {
+    /**
+     * Menentukan apakah suatu bilangan (factor) merupakan faktor dari bilangan lainnya (base)
+     * @param base variable untuk bilangan dasar
+     * @param factor variable untuk bilangan faktor
+     * @return true jika factor adalah faktor dari base, sebaliknya false
+     */
+    public static boolean checkForFactor(int base, int factor) {
+        return base % factor == 0;
+    }
+
+//    public static void main(String[] args) {
+//        System.out.println(checkForFactor(7, 2));
+//    }
+}
